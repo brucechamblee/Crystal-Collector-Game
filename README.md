d1f970acf1062e5bf44b305b266233747f69d24a
bruce chamblee <brucechamblee@gmail.com>
2019-03-16 22:20:00 -0400

    last updated

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # unit-4-game
+Made the crystal collector game. In this game, you the user will be given a random number which the computer will generate. Next, the user will click on the crystals below which all have a random number nested inside of them. Each time the user clicks the crystal, the number nested inside the crystal will be added to the users count. If the user can make their number match the radomly guessed number, they win. If they exceed the randomly generated number, they lose.
+
+Next they will click on any crystal to reset the game. A new random number will be calculated and each crystal will generate a new number. 
+
+## HTML
+
+The HTML in this project was very basic. I created a container which held the following:
+Header Row -  image and box element to store the text.
+Directions Row - contains the directions for the user.
+Numbers Row - This row contained the randmon number box and the Wins/Loses Box.
+Crystal Row - This row held the crystal images.
+Score Row - This row contained the users clicked score.
+
+Things to mention was that I linked Jquery in this sheet. 
+
+## CSS
+I used a reset.css file and a Bootstrap file for the grid. I next linked my own CSS Style Sheet to help match the colors of the project. Whoever did the template was quite colorful.
+
+## JavaScript
+In the Javascript, I started out by Psuedo Coding my work. This was very helpful.
+
+Variables -  I started with creating my variables. One for wins, loses, and final score. 
+
+Random Number - I made a variable that would hold my random generated number. I then attached a function to create this number and text it to the HTML element. 
+
+Gem Variables - I created my crystal variables and like before, I set them to generate a number between 1 and 12.
+
+Reset - I made a Reset Function to reset the variables and generate the new numbers when the round was finished. 
+
+Win Function - Set it to alert the user they won, add a 1 to the wins, and the screen, and reset the game. 
+
+Lose Function - Set it to alert the user they lost, add a 1 to the loses, and the screen, and reset the game. 
+
+Events - Lastly, I created the onclick events which would cause the numbers to be added to the users final score and run and if/else statement for win/lose checking. 
+
+
+
+
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
 var wins = 0
 // create another empty array for the loses with only ""
 var loses = 0
-
-var total = 0
+// create a variable to hold the total crystal count.
+var finalScore = 0;
 
 //make the system get a random number - use math.random?
-var randomNum = Math.floor((Math.random() * 120) + 19);
+var randomNum = Math.floor((Math.random() * (120 - 19 + 1)) + 19);
 $("#randomnum").text(randomNum);
 
 //make a function that will add the score of each click to add to the total score.
@@ -17,31 +17,88 @@ var randomGem2 = Math.floor((Math.random() * 12) + 1);
 var randomGem3 = Math.floor((Math.random() * 12) + 1);
 var randomGem4 = Math.floor((Math.random() * 12) + 1);
 
-console.log(randomGem1);
-console.log(randomGem2);
-console.log(randomGem3);
-console.log(randomGem4);
-console.log(randomNum)
 
 
+// game reset
+function reset(){
+    var randomNum = Math.floor((Math.random() * (120 - 19 + 1)) + 19);
+    $("#randomnum").text(randomNum);
+    randomGem1 = Math.floor((Math.random() * 12) + 1);
+    randomGem2 = Math.floor((Math.random() * 12) + 1);
+    randomGem3 = Math.floor((Math.random() * 12) + 1);
+    randomGem4 = Math.floor((Math.random() * 12) + 1);
+    finalScore = 0
+    $('#totalscorenumber').text(finalScore);
+    }
+
+    // function to add wins
+    function win(){
+        alert("you won");
+        wins++;
+        $('#wins').text(wins);
+        reset();
+    }
+
+    function lose(){
+        alert ("you lose");
+        loses++;
+        $('#loses').text(loses);
+        reset();
+    }
 
     
 
     //make an on click function that makes each crystal pick a random number.
-    $("#gem1").on("click", function(randomGem1) {
-    console.log(randomGem1);
+    $("#gem1").on("click", function() {
+        finalScore = finalScore + randomGem1;
+        console.log(finalScore);
+        console.log(randomGem1);
+        $("#totalscorenumber").text(finalScore);
+            if (finalScore === randomNum){
+                win();
+            }
+            else if ( finalScore > randomNum){
+                lose();
+            }
     });
     
     $("#gem2").on("click", function() {
-        var randomGem2 = Math.floor((Math.random() * 12) + 1);
+        finalScore = finalScore + randomGem2;
+        console.log(finalScore);
+        console.log(randomGem2);
+        $("#totalscorenumber").text(finalScore);
+            if (finalScore === randomNum){
+                win();
+            }
+            else if ( finalScore > randomNum){
+                lose();
+            }
     });
 
     $("#gem3").on("click", function() {
-        var randomGem3 = Math.floor((Math.random() * 12) + 1);
+        finalScore = finalScore + randomGem3;
+        console.log(finalScore);
+        console.log(randomGem3);
+        $("#totalscorenumber").text(finalScore);
+            if (finalScore === randomNum){
+                win();
+            }
+            else if ( finalScore > randomNum){
+                lose();
+            }
         });
 
     $("#gem4").on("click", function() {
-        var randomGem4 = Math.floor((Math.random() * 12) + 1);
+        finalScore = finalScore + randomGem4;
+        console.log(finalScore);
+        console.log(randomGem4);
+        $("#totalscorenumber").text(finalScore);
+            if (finalScore === randomNum){
+                win();
+            }
+            else if ( finalScore > randomNum){
+                lose();
+            }
         });
 
 });
